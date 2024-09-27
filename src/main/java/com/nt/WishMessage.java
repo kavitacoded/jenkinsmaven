149be491102f682ff20af72119a116a5016f5ce8
kavitacoded <kavi20900@gmail.com>
2024-09-27 14:46:38 +0530

    jenins

---
+++ src/main/java/com/nt/WishMessage.java
@@ -0,0 +1,28 @@
+package com.nt;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet
+public class WishMessage extends HttpServlet {
+
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pw=resp.getWriter();
+		pw.println("<h1 style='color:red;text-align:center'>Good morning</h1>");
+		pw.println("<br><br><a href='index.jsp'>Home</a>");
+		super.doGet(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		super.doPost(req, resp);
+	}
+}
